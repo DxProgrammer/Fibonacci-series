5452c78b01fee21a519c5915b30653791aba8660
Titan <Titan@TITAN>
2024-03-05 10:03:53 +0530

    Add project files.

---
+++ First_program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hello
+{
+    class progrem
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Enter the number : ");
+            String n = Console.ReadLine();
+            int f = 1;
+            for (int i = 1; i <= int.Parse(n); i++)
+            {
+                f = f * i;
+            }
+            Console.WriteLine(f);
+            Console.ReadLine(); 
+
+        }
+    }
+}
